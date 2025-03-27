096f313c79032fef0198a65ccb4c9d2065f49372
JarlBurget <jarl-ivor.burget@voco.ee>
2025-03-27 15:59:22 +0200

    Demo lehe videod ruudulised

--- src/components/common/UI/DemoVideo.jsx
+++ src/components/common/UI/DemoVideo.jsx
@@ -1,8 +1,8 @@
 const DemoVideo = ({ videoUrl }) => {
   return (
-    <div className="py-4">
+    <div className="py-4 ">
         <iframe
-        className="w-[336px] sm:w-[560px] aspect-video"
+        className="w-[336px] sm:w-[560px] aspect-square"
         src={videoUrl}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
